04f35f891f0445cf6338ff1755c96b6f8e4c0186
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-11-22 15:20:41 +0100

    Split off parse function from tasks

--- prefect_meemoo/rdf.py
+++ prefect_meemoo/rdf.py
@@ -1,14 +1,12 @@
 import os.path
-from collections import deque
 from typing import Any, Optional, Dict
 
-import ijson
 from prefect import get_run_logger, task
-from pyoxigraph import *
-from rdflib import BNode, Graph, Literal, Namespace
+from rdflib import Graph, Namespace
 from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 from SPARQLWrapper import DIGEST, GET, POST, SPARQLWrapper
 from SPARQLWrapper.Wrapper import BASIC
+from rdf_parse import parse_json
 
 AUTH_TYPES = {HTTPBasicAuth: BASIC, HTTPDigestAuth: DIGEST}
 METHODS = {"GET": GET, "POST": POST}
@@ -44,7 +42,7 @@ def sparql_update(
 
     query_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), query)
     if os.path.isfile(query_path):
-        with open(query_path) as f:
+        with open(query_path, encoding="utf-8") as f:
             query = f.read()
     else:
         logger.warning("Query does not point to a file; executing as query text.")
@@ -58,7 +56,7 @@ def sparql_update(
         for h in headers.items():
             sparql.addCustomHttpHeader(h[0], h[1])
 
-    logger.info("Sending query to {}".format(endpoint))
+    logger.info(f"Sending query to {endpoint}")
 
     results = sparql.query()
     logger.info(results.response.read())
@@ -67,11 +65,11 @@ def sparql_update(
 
 
 @task(name="insert RDF triples")
-def insert(triples, graph=None):
+def insert(triples, endpoint, graph=None):
     query = "INSERT DATA {\n"
 
     if graph is not None:
-        query += "GRAPH <{}> {{\n".format(graph)
+        query += f"GRAPH <{graph}> {{\n"
 
     for t in triples:
         query += to_ntriples(t)
@@ -81,15 +79,13 @@ def insert(triples, graph=None):
     if graph is not None:
         query += "}"
 
-    sparql_update(query)
+    sparql_update(query, endpoint)
 
 
 def to_ntriples(t, namespace_manager=None):
-    return "{} {} {} . \n".format(
-        t[0].n3(namespace_manager),
-        t[1].n3(namespace_manager),
-        t[2].n3(namespace_manager),
-    )
+    return (f"{t[0].n3(namespace_manager)} " 
+            f"{t[1].n3(namespace_manager)} " 
+            f"{t[2].n3(namespace_manager)} . \n")
 
 
 @task(name="convert json to rdf")
@@ -101,133 +97,29 @@ def json_to_rdf(input_data, ns=SRC_NS):
 
 
 @task(name="sparql transformation")
-def sparql_transform(input_data, query_location):
-    temp_input_graph = Graph(store="Oxigraph")
-    temp_output_graph = Graph(store="Oxigraph")
+def sparql_transform(input_data, query):
+    logger = get_run_logger()
+    input_graph = Graph(store="Oxigraph")
+    output_graph = Graph(store="Oxigraph")
 
-    query_file = open(query_location, "r")
-    query = query_file.read()
-    query_file.close()
+    query_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), query)
+    if os.path.isfile(query_path):
+        with open(query_path, encoding="utf-8") as f:
+            query = f.read()
+    else:
+        logger.warning("Query does not point to a file; executing as query text.")
 
-    temp_input_graph.parse(data=input_data, format="nt")
+    input_graph.parse(data=input_data, format="nt")
 
-    results = temp_input_graph.query(query)
+    results = input_graph.query(query)
 
     for result in results:
-        temp_output_graph.add(result)
+        output_graph.add(result)
 
-    return temp_output_graph.serialize(format="nt")
+    return output_graph.serialize(format="nt")
 
 
 @task(name="concatenate ntuples")
 def combine_ntuples(*ntuples):
     temp = "\n".join(ntuples)
     return temp
-
-
-def parse_dict(data, **kwargs):
-    """Generates RDFlib triples from a python dictionary using a direct mapping."""
-
-    def basic_parse(data):
-        if isinstance(data, dict):  # start_map
-            yield "start_map", None
-            for k, v in data.items():
-                yield "map_key", k
-                for event, value in basic_parse(v):
-                    yield event, value
-            yield "end_map", None
-        elif isinstance(data, list):  # start_list
-            yield "start_array", None
-            for i in data:
-                for event, value in basic_parse(i):
-                    yield event, value
-            yield "end_array", None
-        elif data is None:
-            yield "null", data
-        elif isinstance(data, str):
-            yield "string", data
-        elif isinstance(data, bool):
-            yield "boolean", data
-        elif isinstance(data, int):
-            yield "integer", data
-        elif isinstance(data, float):
-            yield "double", data
-
-    events = basic_parse(data)
-    return _parse_events(events, **kwargs)
-
-
-def parse_json(json, **kwargs):
-    """
-    Generates RDFlib triples from a file-like object
-    or a string using a direct mapping.
-    """
-
-    #   parse json
-    events = ijson.basic_parse(json, use_float=True)
-
-    return _parse_events(events, **kwargs)
-
-
-def _parse_events(events, **kwargs):
-    """
-    Internal method that generates RDFlib triples
-    from a generator function that yields event, value pairs.
-    """
-
-    # initalize defaults
-    namespace = Namespace("http://localhost/")
-    instance_ns = None
-
-    if "namespace" in kwargs and isinstance(kwargs["namespace"], Namespace):
-        namespace = kwargs["namespace"]
-
-    if "instance_ns" in kwargs and isinstance(kwargs["instance_ns"], Namespace):
-        instance_ns = kwargs["instance_ns"]
-
-    # initializing deque
-    subjectStack = deque([])
-    arrayProperties = {}
-    prop = None
-
-    i = 0
-    for event, value in events:
-        if event == "start_array" and subjectStack and prop is not None:
-            # fetching the last subject
-            s = subjectStack[-1]
-            arrayProperties[s] = prop
-
-        if event == "end_array" and subjectStack:
-            # fetching the last subject
-            s = subjectStack[-1]
-            arrayProperties.pop(s, None)
-
-        if event == "start_map":
-            if instance_ns is not None:
-                subject = instance_ns[str(i)]
-                i += 1
-            else:
-                subject = BNode()
-            # add triple with current array property, if any
-            if prop is not None and subjectStack:
-                # fetching the last subject
-                s = subjectStack[-1]
-                yield (s, prop, subject)
-            subjectStack.append(subject)
-
-        if event == "end_map":
-            subjectStack.pop()
-
-            # restore previous array property, if there was any
-            if subjectStack and subjectStack[-1] in arrayProperties:
-                prop = arrayProperties[subjectStack[-1]]
-
-        if event in ["boolean", "integer", "double", "number"]:
-            yield (subjectStack[-1], prop, Literal(value))
-
-        if event == "string" and prop is not None:
-            yield (subjectStack[-1], prop, Literal(value))
-            # yield (subjectStack[-1], property, Literal(value, datatype=XSD.string))
-
-        if event == "map_key":
-            prop = namespace[value]
