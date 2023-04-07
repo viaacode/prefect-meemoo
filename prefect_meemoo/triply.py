3e65f5cbca7a18312a1ab7dee4112cb7093cd1e8
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-04-07 11:55:49 +0200

    Parse TriplyETL output for logging

--- prefect_meemoo/triply.py
+++ prefect_meemoo/triply.py
@@ -4,6 +4,7 @@ import re
 from pathlib import Path
 
 from prefect import get_run_logger, task
+from prefect.states import Failed
 
 
 @task(name="Run TriplyETL", description="Runs an TriplyETL script.")
@@ -18,37 +19,59 @@ def run_triplyetl(etl_script_path: str, **kwargs: str):
     etl_env = os.environ.copy()
     for key, value in kwargs.items():
         etl_env[key.upper()] = value
-    
+
     p = subprocess.Popen(
         ["yarn", "ratt", etl_script_abspath],
         cwd=etl_folder_abspath,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
-        env=etl_env
+        env=etl_env,
+        encoding="utf-8",
     )
 
     p.wait()
 
-    if p.returncode > 0:
-        raise Exception("Running TriplyETL failed.")
+    # Parse CLI output from TriplyETL for logging
+    record_message = False
+    error = False
+    message = ""
+    while True:
+        line = p.stdout.readline()
 
-    out = p.stdout.read()
-    logger.info(out)
+        # Start recording log message when encountering start frame
+        if re.search(r"╭─|┌─", line):
+            record_message = True
 
+        # Start recording error message when encountering ERROR
+        if re.search(r"ERROR", line):
+            record_message = True
+            error = True
+
+        if record_message:
+            message += line
+
+        # Stop recording log message when encountering end frame
+        if re.search(r"╰─|└─", line):
+            if error:
+                logger.error(message)
+            else:
+                logger.info(message)
+            record_message = False
+            error = True
+            message = ""
+
+        if not line:
+            break
+
+    # Split and log stderr in warning and error
     for err in p.stderr.readlines():
-        if re.match(r'warning', err):
+        if re.match(r"warning", err):
             logger.warning(err)
         else:
             logger.error(err)
 
-    # while True:
-    #     line = p.stdout.readline()
-    #     logger.info(repr(line))
-
-    #     if not line:
-    #         break
+    if p.returncode > 0:
+        return Failed()
 
     return p.returncode > 0
-
-
