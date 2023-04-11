3e9478ad13acc7945b892a222ac7a973a0a63adb
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-04-11 15:44:22 +0200

    Improve unittests

--- tests/test_triplyetl.py
+++ tests/test_triplyetl.py
@@ -1,6 +1,16 @@
 import pytest
 from prefect import flow
+from prefect.blocks.core import Block
+from rdflib import Graph
+from rdflib.compare import to_isomorphic
+
 from prefect_meemoo.triply import run_triplyetl
+
+
+def rdf_is_equal(expected, actual):
+    g_expected = Graph().parse(data=expected)
+    g_actual = Graph().parse(file=actual)
+    return to_isomorphic(g_expected) == to_isomorphic(g_actual)
 
 
 def test_run_triplyetl():
@@ -9,7 +19,38 @@ def test_run_triplyetl():
         run_triplyetl(etl_script_path="./tests/etl/dist/index.js")
 
     result = test_flow()
+
+    expected = """
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.
+
+    <https://data.hetarchief.be/id/OR-xxxxx> a <https://schema.org/Person>;
+    <https://schema.org/name> "xx".
+    """
+
     assert result
+    assert rdf_is_equal(expected, "./etl/static/output.ttl")
+
+
+def test_add_block_as_variables():
+    @flow(name="prefect_flow_triplyetl")
+    def test_flow():
+        run_triplyetl(
+            etl_script_path="./tests/etl/dist/variables_block.js",
+            triply=Block(test="test"),
+        )
+
+    result = test_flow()
+
+    expected = """
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.
+
+    <https://data.hetarchief.be/id/Jane> rdfs:label "test-block".
+    """
+
+    assert result
+    assert rdf_is_equal(expected, "./etl/static/output-block.ttl")
 
 
 def test_run_triplyetl_with_variables():
@@ -20,7 +61,16 @@ def test_run_triplyetl_with_variables():
         )
 
     result = test_flow()
+
+    expected = """
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.
+
+    <https://data.hetarchief.be/id/Jane> rdfs:label "test-value".
+    """
+
     assert result
+    assert rdf_is_equal(expected, "./etl/static/output-variables.ttl")
 
 
 def test_run_triplyetl_with_error():
