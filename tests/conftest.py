fcf5ef97dc4839c54b136b44572ef35a87b6b6bc
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-04-13 11:11:29 +0200

    Remove --ignore-engines because of yarn 3.5

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ import subprocess
 def pytest_sessionstart():
     path = os.path.join(os.getcwd(), "tests/etl")
     subprocess.run(['yarn', 'cache', 'clean'], cwd=path, check = True)
-    subprocess.run(['yarn', 'install', '--ignore-engines'], cwd=path, check = True)
+    subprocess.run(['yarn', 'install'], cwd=path, check = True)
     subprocess.run(['yarn', 'tsc'], cwd=path, check = True)
