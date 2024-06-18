a6138ff0556eb6c606179c87dd4165e5e40c31e4
lennertvandevelde <lennert.van.de.velde@outlook.be>
2024-06-18 16:19:00 +0200

    fix timezone

--- prefect_meemoo/config/blocks.py
+++ prefect_meemoo/config/blocks.py
@@ -24,7 +24,7 @@ class LastRunConfig(Block):
 
     _block_type_name = "Last Run Config"
     _logo_url = "https://cdn-icons-png.flaticon.com/512/8766/8766995.png"
-    last_run: str = Field(default=flow_run.get_scheduled_start_time().to_iso8601_string(), description="The last time the flow started.")
+    last_run: str = Field(default=flow_run.get_scheduled_start_time().astimezone().to_iso8601_string(), description="The last time the flow started.")
     last_run_dict: dict = Field({}, description="Dictionary containing dates based on a certain value for a flow.")
     flow_name: str = Field(default=(...), description="The name of the flow.")
     name: str = Field(default=(...), description="The name of the deployment.")
@@ -41,12 +41,12 @@ class LastRunConfig(Block):
     def add_last_run(self, context: str = "", time: pendulum.DateTime= None):
         if context:
             if not time:
-                self.last_run_dict[context] = flow_run.get_scheduled_start_time().to_iso8601_string()
+                self.last_run_dict[context] = flow_run.get_scheduled_start_time().astimezone().to_iso8601_string()
             else:
                 self.last_run_dict[context] = time.to_iso8601_string()
         else:
             if not time:
-                self.last_run = flow_run.get_scheduled_start_time().to_iso8601_string()
+                self.last_run = flow_run.get_scheduled_start_time().astimezone().to_iso8601_string()
             else:
-                self.last_run = time.to_iso8601_string()
+                self.last_run = time.astimezone().to_iso8601_string()
         self.save(name=self.name, overwrite=True)
