787b92e961d8099cc025963042b3917c951a311a
lennertvandevelde <lennert.van.de.velde@outlook.be>
2024-06-21 11:27:10 +0200

    Fix test service

--- tests/test_services.py
+++ tests/test_services.py
@@ -12,30 +12,33 @@ def mocked_requests_get(*args, **kwargs):
     global get_request_count
     get_request_count += 1
     class MockResponse:
-        def __init__(self, json_data, status_code):
+        def __init__(self, json_data, status_code, text):
             self.json_data = json_data
             self.status_code = status_code
+            self.text = text
+
 
         def json(self):
             return self.json_data
 
     print("get_request_count: ", get_request_count)
     if get_request_count == 1:
-        return MockResponse({"job_running": True}, 200)
+        return MockResponse({"job_running": True}, 200, "test post response")
     elif get_request_count == 2:
-        return MockResponse({"job_running": False}, 200)
-    return MockResponse(None, 404)
+        return MockResponse({"job_running": False}, 200, "test post response")
+    return MockResponse(None, 404, "test post response")
 
 def mocked_requests_post(*args, **kwargs):
     class MockResponse:
-        def __init__(self, json_data, status_code):
+        def __init__(self, json_data, status_code, text):
             self.json_data = json_data
             self.status_code = status_code
+            self.text = text
 
         def json(self):
             return self.json_data
 
-    return MockResponse(None, 200)
+    return MockResponse(None, 200, "test post response")
 
 def mocked_get_run_logger():
     class MockLogger:
