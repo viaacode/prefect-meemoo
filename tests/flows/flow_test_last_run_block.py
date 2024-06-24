2c109e671261ef881da86a0bc864fe8835c01a48
lennertvandevelde <lennert.van.de.velde@outlook.be>
2024-06-24 10:14:33 +0200

    Fix logs

--- tests/flows/flow_test_last_run_block.py
+++ tests/flows/flow_test_last_run_block.py
@@ -12,22 +12,22 @@ def main_flow():
     logger = get_run_logger()
 
     last_run = get_last_run_config()
-    logger.info("last_run: ", last_run)
+    logger.info(f"last_run: {last_run}")
     assert last_run != None
 
     context_last_run = get_last_run_config(context="context")
-    logger.info("context_last_run: ", context_last_run)
+    logger.info(f"context_last_run: {context_last_run}")
     assert context_last_run != None
 
     context_last_run_2000 = get_last_run_config(context="context_2000")
-    logger.info("context_last_run_2000: ", context_last_run_2000)
+    logger.info(f"context_last_run_2000: ${context_last_run_2000}")
     assert context_last_run_2000 == pendulum.datetime(2000, 1, 1, 10).to_iso8601_string()
 
     add_last_run_with_context("context")
     add_last_run_with_context("context_2000", pendulum.datetime(2000, 1, 1, 10))
 
     context_last_run_2 = get_last_run_config(context="context")
-    logger.info("context_last_run_2: ", context_last_run_2)
+    logger.info(f"context_last_run_2: {context_last_run_2}")
     assert context_last_run_2 != None
 
 
